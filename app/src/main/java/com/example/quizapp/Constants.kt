88cf9c0194d10287e272b27491c5370f457b4052
Oguzhan Secgel <oguzhansecgel19@icloud.com>
2023-06-04 15:33:17 +0300

    Selecting the right and wrong answer and displaying the next question

--- app/src/main/java/com/example/quizapp/Constants.kt
+++ app/src/main/java/com/example/quizapp/Constants.kt
@@ -27,9 +27,72 @@ object Constants {
             3,"What country does this flag belong to?",
             R.drawable.ic_flag_of_australia,
             "ABD","Australia","Germany","Syria",
-            4
+            2
         )
         questionList.add(que3)
+
+        val que4 = Question(
+            4,"What country does this flag belong to?",
+            R.drawable.ic_flag_of_brazil,
+            "France","Japan","Brazil","Kuwai",
+            3
+        )
+        questionList.add(que4)
+        val que5 = Question(
+            5,"What country does this flag belong to?",
+            R.drawable.ic_flag_of_bolivia,
+            "Bangladesh","Chile","Bolivia","Cameroon",
+            3
+        )
+        questionList.add(que5)
+        val que6 = Question(
+            6,"What country does this flag belong to?",
+            R.drawable.ic_flag_of_denmark,
+            "Canada","Denmark","Czech Republic","Austria",
+            2
+        )
+        questionList.add(que6)
+        val que7 = Question(
+            7, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_fiji,
+            "Indonesia", "Germany", "Fiji", "France",
+            3
+        )
+        questionList.add(que7)
+
+        val que8 = Question(
+            8, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_germany,
+            "Brazil", "Denmark", "Germany", "India",
+            3
+        )
+        questionList.add(que8)
+
+        val que9 = Question(
+            9, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_india,
+            "Kuwait", "New Zealand", "India", "Australia",
+            3
+        )
+        questionList.add(que9)
+
+        val que10 = Question(
+            10, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_kuwait,
+            "France", "Kuwait", "Germany", "Brazil",
+            2
+        )
+        questionList.add(que10)
+
+        val que11 = Question(
+            11, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_new_zealand,
+            "Spain", "Japan", "New Zealand", "China",
+            3
+        )
+        questionList.add(que11)
+
+        questionList.shuffle() // karıştırma fonksiyonu .
         return questionList
     }
 
--- app/src/main/java/com/example/quizapp/QuizQeustionsActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQeustionsActivity.kt
@@ -2,14 +2,12 @@ package com.example.quizapp
 
 import android.graphics.Color
 import android.graphics.Typeface
+import android.graphics.drawable.AdaptiveIconDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.ProgressBar
-import android.widget.TextView
+import android.widget.*
 import androidx.core.content.ContextCompat
 
 class QuizQeustionsActivity : AppCompatActivity(), View.OnClickListener {
@@ -57,11 +55,11 @@ class QuizQeustionsActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun setQuestion() {
-
+        defaultOptionView()
         val question: Question = mQuestionsList!![mCurrentPosition - 1]
         ivImage?.setImageResource(question.image)
         progressBar?.progress = mCurrentPosition
-        tvProgress?.text = "$mCurrentPosition/ ${progressBar?.max}"
+        tvProgress?.text = "$mCurrentPosition/ ${mQuestionsList?.size}"
         tvQuestion?.text = question.question
         tvOptionOne?.text = question.optionOne
         tvOptionTwo?.text = question.optionTwo
@@ -143,8 +141,73 @@ class QuizQeustionsActivity : AppCompatActivity(), View.OnClickListener {
            }
            R.id.btn_submit->
            {
-               // TODO "implement btn submit"
+               if(mSelectedOptionPosition ==0)
+               {
+                   mCurrentPosition++
+
+
+                   when{
+                       mCurrentPosition<= mQuestionsList!!.size->
+                       {
+                           setQuestion()
+                       }
+                       else ->{
+                           Toast.makeText(this, "You Made it to the end", Toast.LENGTH_SHORT).show()
+                       }
+
+                   }
+               }
+               else
+               {
+                   val question = mQuestionsList?.get(mCurrentPosition-1)
+                   if(question!!.correctAnswer!=mSelectedOptionPosition)
+                   {
+                       answerView(mSelectedOptionPosition,R.drawable.wrong_option_border_bg)
+                   }
+                       answerView(question.correctAnswer,R.drawable.correct_option_border_bg)
+
+                   if(mCurrentPosition == mQuestionsList!!.size)
+                   {
+                       btnSubmit?.text="FINISH"
+                   }
+                   else
+                   {
+                       btnSubmit?.text="GO TO NEXT QUESTION"
+                   }
+                   mSelectedOptionPosition=0
+               }
            }
        }
+    }
+
+    private fun answerView(ansver: Int,drawableView:Int)
+    {
+        when(ansver)
+        {
+            1 ->{
+                tvOptionOne?.background = ContextCompat.getDrawable(
+                    this,
+                    drawableView
+                )
+            }
+            2 ->{
+                tvOptionTwo?.background = ContextCompat.getDrawable(
+                    this,
+                    drawableView
+                )
+            }
+            3 ->{
+                tvOptionThree?.background = ContextCompat.getDrawable(
+                    this,
+                    drawableView
+                )
+            }
+            4 ->{
+                tvOptionFour?.background = ContextCompat.getDrawable(
+                    this,
+                    drawableView
+                )
+            }
+        }
     }
 }
